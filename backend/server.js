764e583bc699ddae3e8ef5c6ff7792270c411724
Anuragp-0407 <pandeyanurag0407@gmail.com>
2026-08-31 21:51:27 +0530

    connect mongoDb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const dotenv = require("dotenv");
 const app = require("./src/app");
+const connectDB = require("./src/config/db");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
